b830b9af3efcf253722db1ef3266ea78da4bb407
adilhayat86 <adilhayat@yahoo.com>
2026-05-28 00:29:26 +0500

    feat: add firestore collection names

---
+++ src/firebase/collectionNames.ts
@@ -0,0 +1,8 @@
+export const FIRESTORE_COLLECTIONS = {
+  users: 'users',
+  listings: 'listings',
+  requirements: 'requirements',
+  comments: 'comments',
+  agents: 'agents',
+  notifications: 'notifications',
+} as const;
